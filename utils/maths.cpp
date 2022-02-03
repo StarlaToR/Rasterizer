9746573d869eb82339206d7e95a5282a6212210e
Antoine MORDANT <a.mordant@student.isartdigital.com>
2022-02-03 10:04:55 +0100

    update

--- utils/maths.cpp
+++ utils/maths.cpp
@@ -116,7 +116,9 @@ Mat4 Mat4::CreateTransformMatrix(const Vec3& rotation, const Vec3& position, con
     Mat4 rotateZ  = CreateZRotationMatrix(rotation.z);
     Mat4 scaling = CreateScaleMatrix(scale);
 
-    Mat4 transform = translation * rotateY;
+    Mat4 transform = getIdentityMat4();
+    transform *= translation;
+    transform *= rotateY;
     transform *= rotateX;
     transform *= rotateZ;
     transform *= scaling;
@@ -149,7 +151,7 @@ static Mat4 getIdentityMat4()
 
 static Mat4 CreateTranslationMatrix(const Vec3& translation)
 {
-    Mat4 matrix = Mat4();
+    Mat4 matrix = getIdentityMat4();
     matrix.tab[0][3] = translation.x;
     matrix.tab[1][3] = translation.y;
     matrix.tab[2][3] = translation.z;
@@ -158,7 +160,7 @@ static Mat4 CreateTranslationMatrix(const Vec3& translation)
 
 static Mat4 CreateScaleMatrix(const Vec3& scale)
 {
-    Mat4 matrix = Mat4();
+    Mat4 matrix = getIdentityMat4();
     matrix.tab[0][0] = scale.x;
     matrix.tab[1][1] = scale.y;
     matrix.tab[2][2] = scale.z;
@@ -167,7 +169,7 @@ static Mat4 CreateScaleMatrix(const Vec3& scale)
 
 static Mat4 CreateXRotationMatrix(float angle)
 {
-    Mat4 matrix = Mat4();
+    Mat4 matrix = getIdentityMat4();
     matrix.tab[1][1] = cos(angle);
     matrix.tab[1][2] = -sin(angle);
     matrix.tab[2][1] = sin(angle);
@@ -177,7 +179,7 @@ static Mat4 CreateXRotationMatrix(float angle)
 
 static Mat4 CreateYRotationMatrix(float angle)
 {
-    Mat4 matrix = Mat4();
+    Mat4 matrix = getIdentityMat4();
     matrix.tab[0][0] = cos(angle);
     matrix.tab[0][2] = sin(angle);
     matrix.tab[2][0] = -sin(angle);
@@ -187,7 +189,7 @@ static Mat4 CreateYRotationMatrix(float angle)
 
 static Mat4 CreateZRotationMatrix(float angle)
 {
-    Mat4 matrix = Mat4();
+    Mat4 matrix = getIdentityMat4();
     matrix.tab[0][0] = cos(angle);
     matrix.tab[0][1] = -sin(angle);
     matrix.tab[1][0] = sin(angle);
