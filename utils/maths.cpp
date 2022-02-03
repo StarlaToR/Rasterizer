4d65372254fc2701cca44a6c2fb1c4e98c6fcc7b
Antoine MORDANT <a.mordant@student.isartdigital.com>
2022-02-03 09:54:14 +0100

    update

--- utils/maths.cpp
+++ utils/maths.cpp
@@ -137,9 +137,19 @@ void Mat4::PrintMatrix()
     printf("\n\n");
 }
 
+static Mat4 getIdentityMat4()
+{
+    Mat4 matrix = Mat4();
+    matrix.tab[0][0] = 1;
+    matrix.tab[1][1] = 1;
+    matrix.tab[2][2] = 1;
+    matrix.tab[3][3] = 1;
+    return matrix;
+}
+
 static Mat4 CreateTranslationMatrix(const Vec3& translation)
 {
-    Mat4 matrix = GetIdentityMat4();
+    Mat4 matrix = Mat4();
     matrix.tab[0][3] = translation.x;
     matrix.tab[1][3] = translation.y;
     matrix.tab[2][3] = translation.z;
@@ -148,7 +158,7 @@ static Mat4 CreateTranslationMatrix(const Vec3& translation)
 
 static Mat4 CreateScaleMatrix(const Vec3& scale)
 {
-    Mat4 matrix = GetIdentityMat4();
+    Mat4 matrix = Mat4();
     matrix.tab[0][0] = scale.x;
     matrix.tab[1][1] = scale.y;
     matrix.tab[2][2] = scale.z;
@@ -157,7 +167,7 @@ static Mat4 CreateScaleMatrix(const Vec3& scale)
 
 static Mat4 CreateXRotationMatrix(float angle)
 {
-    Mat4 matrix = GetIdentityMat4();
+    Mat4 matrix = Mat4();
     matrix.tab[1][1] = cos(angle);
     matrix.tab[1][2] = -sin(angle);
     matrix.tab[2][1] = sin(angle);
@@ -167,7 +177,7 @@ static Mat4 CreateXRotationMatrix(float angle)
 
 static Mat4 CreateYRotationMatrix(float angle)
 {
-    Mat4 matrix = GetIdentityMat4();
+    Mat4 matrix = Mat4();
     matrix.tab[0][0] = cos(angle);
     matrix.tab[0][2] = sin(angle);
     matrix.tab[2][0] = -sin(angle);
@@ -177,7 +187,7 @@ static Mat4 CreateYRotationMatrix(float angle)
 
 static Mat4 CreateZRotationMatrix(float angle)
 {
-    Mat4 matrix = GetIdentityMat4();
+    Mat4 matrix = Mat4();
     matrix.tab[0][0] = cos(angle);
     matrix.tab[0][1] = -sin(angle);
     matrix.tab[1][0] = sin(angle);
