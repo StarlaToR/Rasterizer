17b1418fd067488152a2e066225d0076b972253a
m.jammet <m.jammet@student.isartdigital.com>
2022-02-16 13:28:43 +0100

    push pour Antoine

--- include/maths.hpp
+++ include/maths.hpp
@@ -202,8 +202,9 @@ Vec3 operator-(const Vec3& a, const Vec3& b);
 
 inline Vec3 GetNormalVector(const Vec3& p0, const Vec3& p1, const Vec3& p2)
 {
-    Vec3 normal = (p1 - p0) * (p2 - p0);
-    //printf("normal={%f, %f, %f, %f}\n",normal.x,normal.y,normal.z);
+    Vec3 normal = (p1-p0)*(p2-p0);
+    normal.Normalize();
+    printf("normal={%f, %f, %f}\n",normal.x,normal.y,normal.z);
     return normal;
 }
 
